47b3f3b22b4771621697a24b81f8219ecae2e3f7
emanuelzh <emanuelz@emanuelz.com.mx>
2015-05-30 14:43:26 -0500

    ya esta el mapa, algo de css y mamadas

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@ var map;
 
 function initialize() {
   var mapOptions = {
-    zoom: 18,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 14,
+    center: new google.maps.LatLng(20.5805905,-100.3998285)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 //jquery dom
 $(document).ready(function(){
-
 	//inicializa el pinche mapa
 	initialize();
-
 });
